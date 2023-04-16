e5a13dd0101aa48ce382e4c95fb334b31c8384d2
junhyukPark <junni802@naver.com>
2023-04-16 11:06:27 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleaf/ThymeleadBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleadBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleadBasicApplication.class, args);
+    }
+
+}
